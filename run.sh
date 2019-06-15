ca4a88097dded8135ca88af73cb2266a5e2eb92a
david cochran <about.trout@gmail.com>
2019-06-15 20:32:47 +0000

    simplify run script

--- run.sh
+++ run.sh
@@ -3,13 +3,8 @@
 image_name=smellycat
 
 # Build a fresh smellycat container
-docker rmi -f $image_name
 docker build -t $image_name .
-
 # Make sure all old smellycats are killed
-for box in $(docker ps -q --filter="ancestor=$image_name"); do
-  docker kill $box;
-done
-
+docker kill $(docker ps -q --filter="ancestor=$image_name")
 # Start a new smellycat
 docker run --restart=always -t $image_name
